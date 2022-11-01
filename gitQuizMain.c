d7da681ce9502e86d83f65228256ec196ef36082
BASHI3R <117200333+BASHI3R@users.noreply.github.com>
2022-11-01 13:19:03 -0400

    New patch
    I added new else statement

--- gitQuizMain.c
+++ gitQuizMain.c
@@ -10,7 +10,9 @@ int main(void)
     if(val == 'y'){
         printf("You know how to use Git!");
     }
-
+    else(val == 'n'){
+        printf("why no?");
+    }
     printf("\n");
 
     return 0;
